322c7884f0fae69414517ae15b2725efa9e9a8d0
teetruc <ttruc7290@sdsu.edu>
2024-01-19 11:49:11 -0800

    Add files via upload

---
+++ src/store/results.js
@@ -0,0 +1,38 @@
+import Vue from "vue";
+const state = {
+    searchParam: '',
+    searchResults: [],
+    bookmarks: JSON.parse(window.localStorage.getItem('bookmarks'))
+}
+const getters = {
+    getSearchResults: state => state.searchResults,
+    getSearchParam: state => state.searchParam,
+    getBookmarks: state => {
+        return state.bookmarks
+    }
+}
+const actions = {
+    async fetchSearchResult ({ commit }, searchItem) {
+         
+        const res = await Vue.axios.get(`https://api.edamam.com/search?q=${searchItem}&app_id=${"8b0cc6ee"}&app_key=${"27461be573cdd8e5e82f48a4d8bf1df1"}&from=0&to=20`)
+        const results = res.data.hits
+        commit('updateSearchResults', results)
+    },
+    async fetchSearchItem ({ commit }, item) {
+        commit('updateSearchItem', item)
+    }
+}
+const mutations = {
+    updateSearchResults: (state, results) => {
+        state.searchResults = results
+    },
+    updateSearchItem: (state, item) => {
+        state.searchParam = item
+    }
+}
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
